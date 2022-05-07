fbe346cdbec8233815a88fbba573eceb68c3b3e4
cloversupmnky <cloversupmnky@gmail.com>
2022-05-07 10:55:06 +0800

    新增 Actor Model

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        /// <summary>
+        /// 編號
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 大頭貼路徑
+        /// </summary>
+        public string ProfilePictureURL { get; set; }
+
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// 個人傳記
+        /// </summary>
+        public string Bio { get; set; }
+    }
+}
