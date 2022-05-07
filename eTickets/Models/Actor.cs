58cfa71fc12151c76dfa166620c585987ff66b17
cloversupmnky <cloversupmnky@gmail.com>
2022-05-07 11:20:41 +0800

    新增 Movie Model

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -2,30 +2,15 @@
 
 namespace eTickets.Models
 {
-    /// <summary>
-    /// 演員
-    /// </summary>
     public class Actor
     {
-        /// <summary>
-        /// 編號
-        /// </summary>
         [Key]
         public int Id { get; set; }
 
-        /// <summary>
-        /// 大頭貼路徑
-        /// </summary>
         public string ProfilePictureURL { get; set; }
 
-        /// <summary>
-        /// 名字
-        /// </summary>
         public string FullName { get; set; }
 
-        /// <summary>
-        /// 傳記
-        /// </summary>
         public string Bio { get; set; }
     }
 }
